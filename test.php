ced6f61d942d28736c06fe00123103d2e22af0d0
KNisansala <kavini@sublime.lk>
2018-12-07 16:05:49 +0530

    #239 Blog Design Change Ask A Question & Show More

--- test.php
+++ test.php
@@ -1,39 +1,61 @@
-<?php
-include_once(dirname(__FILE__) . '/class/include.php');
-
-
-?>
+<!doctype html>
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-       <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-md-10">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>jQuery Read More/Less Toggle Example</title>
+<!--<link href="https://www.jqueryscript.net/css/jquerysctipttop.css" rel="stylesheet" type="text/css">-->
+<!--<link href='https://fonts.googleapis.com/css?family=Quicksand' rel='stylesheet' type='text/css'>-->
 
-                    <h3 id="hidden-elements">Hidden elements</h3>
-                    <p>Facebook style, only show a few images but have a large gallery</p>
-                    <div class="row justify-content-center" data-code="example-9">
-                        <a href="https://unsplash.it/1200/768.jpg?image=263" data-toggle="lightbox" data-gallery="hidden-images" class="col-4">
-                            <img src="https://unsplash.it/600.jpg?image=263" class="img-fluid">
-                        </a>
-                        <a href="https://unsplash.it/1200/768.jpg?image=264" data-toggle="lightbox" data-gallery="hidden-images" class="col-4">
-                            <img src="https://unsplash.it/600.jpg?image=264" class="img-fluid">
-                        </a>
-                        <a href="https://unsplash.it/1200/768.jpg?image=265" data-toggle="lightbox" data-gallery="hidden-images" class="col-4">
-                            <img src="https://unsplash.it/600.jpg?image=265" class="img-fluid">
-                        </a>
-                        <!-- elements not showing, use data-remote -->
-                        <div data-toggle="lightbox" data-gallery="hidden-images" data-remote="https://unsplash.it/1200/768.jpg?image=264" data-title="Hidden item 1"></div>
-                        <div data-toggle="lightbox" data-gallery="hidden-images" data-remote="https://unsplash.it/1200/768.jpg?image=265" data-title="Hidden item 3"></div>
-                        <div data-toggle="lightbox" data-gallery="hidden-images" data-remote="https://unsplash.it/1200/768.jpg?image=266" data-title="Hidden item 4"></div>
-                        <div data-toggle="lightbox" data-gallery="hidden-images" data-remote="https://unsplash.it/1200/768.jpg?image=267" data-title="Hidden item 5"></div>
-                    </div>
-                    <p class="footer"><span>Built by me, <a href="https://ashleyd.ws">ashleydw</a></span></p>
-                </div>
-            </div>
-        </div>
-    </body>
+      <style>
+      body { min-height: 100vh;background-image: linear-gradient(60deg, #29323c 0%, #485563 100%); }
+      .container { margin: 150px auto; max-width: 640px; line-height: 1.8; }
+      .box { background-color: #ED5565; padding: 1em; border-radius: 3px; font-family: 'Quicksand'; border-bottom: 10px solid #DA4453; color: #fafafa; }
+      h1 { color: #fff; }
+
+    </style>
+    <link href="css/read-more-less.css" rel="stylesheet" type="text/css"/>
+    <!--<script src="https://cdnjs.cloudflare.com/ajax/libs/prefixfree/1.0.7/prefixfree.min.js"></script>-->
+
+</head>
+
+<body>
+
+    <div class="container">
+      <h1>jQuery Read More/Less Toggle Example</h1>
+      <div class="box">
+    <span class="more">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </span>
+    </div>
+  </div>
+<!--<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>-->
+<script data-cfasync="false" src="../../cdn-cgi/scripts/f2bf09f8/cloudflare-static/email-decode.min.js"></script>
+    <script type="text/javascript" src="scripts/jquery-2.2.0.min.js"></script>
+<script src="css/plugins/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+        <script type="text/javascript" src="scripts/mmenu.min.js"></script>
+        <script type="text/javascript" src="scripts/chosen.min.js"></script>
+        <script type="text/javascript" src="scripts/slick.min.js"></script>
+        <script type="text/javascript" src="scripts/rangeslider.min.js"></script>
+        <script type="text/javascript" src="scripts/magnific-popup.min.js"></script>
+        <script type="text/javascript" src="scripts/waypoints.min.js"></script>
+        <script type="text/javascript" src="scripts/counterup.min.js"></script>
+        <script type="text/javascript" src="scripts/jquery-ui.min.js"></script>
+        <script type="text/javascript" src="scripts/tooltips.min.js"></script>
+        <script type="text/javascript" src="scripts/custom.js"></script>
+        <script src="scripts/add-question.js" type="text/javascript"></script>
+        <script src="lib/sweetalert/sweetalert.min.js" type="text/javascript"></script>
+        <script src="scripts/signin.js" type="text/javascript"></script>
+        <script src="scripts/signup.js" type="text/javascript"></script>
+        <script src="lib/tinymce/js/tinymce/tinymce.min.js" type="text/javascript"></script>
+        <script src="scripts/blog.js" type="text/javascript"></script>
+<script src="scripts/read-more-less.js" type="text/javascript"></script>
+    
+
+
+
+
+</body>
+
 </html>
