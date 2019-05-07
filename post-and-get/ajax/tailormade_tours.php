0a9e11da737fa1c3400e94f98b24d5525d1616b7
MTSUdayanga <sachithgalle@gmail.com>
2019-05-07 09:49:40 +0530

    #489 Send Email To Selected Drivers

--- post-and-get/ajax/tailormade_tours.php
+++ post-and-get/ajax/tailormade_tours.php
@@ -10,7 +10,7 @@ if ($_POST['option'] === 'ADDDETAILS') {
 
     $TAILORMADETOURS->visitor = $_POST['visitor'];
     $TAILORMADETOURS->places = $_POST['places'];
-    $TAILORMADETOURS->driver = $_POST['driver'];
+//    $TAILORMADETOURS->driver = $_POST['driver'];
     $TAILORMADETOURS->no_of_adults = $_POST['noofadults'];
     $TAILORMADETOURS->no_of_children = $_POST['noofchildren'];
     $TAILORMADETOURS->start_date = $_POST['startdate'];
@@ -26,40 +26,40 @@ if ($_POST['option'] === 'ADDDETAILS') {
     $DBOOKING->booking_id = $result->id;
 
     $booking_id = $result->id;
-//    $trim = rtrim($_POST['driver'], ",");
+
     $drivers = $_POST['driver'];
 
     $explode = explode(',', $drivers);
 
-
+//
     $count = 0;
-
+//
     foreach ($explode as $driver_id) {
-//   $result1 = $DBOOKING->create($driver_id,$booking_id);
-
+////   $result1 = $DBOOKING->create($driver_id,$booking_id);
+//
         $cont++;
         if ($cont == 4) {
-            echo 'error';
+//            echo 'error';
         } else {
             $result1 = $DBOOKING->create($driver_id, $booking_id);
+            $senddriveremail = $TAILORMADETOURS->sendBookingConfirmationEmailToDriver($result->id);
         }
     }
 
 
-
-
     if ($result) {
         $sendvisitoremail = $TAILORMADETOURS->sendBookingConfirmationEmailToVisitor($result->id);
-        $senddriveremail = $TAILORMADETOURS->sendBookingConfirmationEmailToDriver($result->id);
+//        $senddriveremail = $TAILORMADETOURS->sendBookingConfirmationEmailToDriver($result->id);
 //        $sendadminemail = $TAILORMADETOURS->sendBookingConfirmationEmailToAdmin($result->id);
 
         unset($_SESSION["destination_cart"]);
 //        if($sendvisitoremail && $senddriveremail && $sendadminemail) {
-        if ($sendvisitoremail && $senddriveremail) {
+        if ($sendvisitoremail) {
             $res = 'TRUE';
         } else {
             $res = 'FALSE';
         }
+       
     }
 
 
